80400a62059d381d61f228d746fae138ccd4515e
elkemper <elkamper@gmail.com>
2016-09-04 18:44:36 +0300

    1

---
+++ ate/ate/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace ate
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
