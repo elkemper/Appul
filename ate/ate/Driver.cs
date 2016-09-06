749f824a08b4ddf2bcd9a4a3ff00e7168e6c817a
Владислав Белоус <vladislav.belous@nforceit.ru>
2016-09-06 17:23:12 +0300

    2

---
+++ ate/ate/Driver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+
+namespace ate
+{
+    public enum BrowsersEnum
+    {
+        chrome,
+        ie
+    }
+
+
+    class Driver
+    {
+        private static readonly string _downloadFileDir;
+
+
+       
+         static Driver()
+        {
+            string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+            _downloadFileDir = String.Format(@"C:\Users\{0}\Downloads", userName);
+        }
+        
+
+        public static string DownloadFileDir
+        {
+            get { return _downloadFileDir; }
+        }
+
+        public static IWebDriver GetDriver(BrowsersEnum browser)
+        {
+            IWebDriver driver = null;
+            switch (browser)
+            {
+                case BrowsersEnum.chrome:
+                    {
+                        driver = new ChromeDriver();
+                        break;
+                    }
+                case BrowsersEnum.ie:
+                    {
+                        driver = new InternetExplorerDriver();
+                            break;
+                    }
+                default:
+                    {
+                        driver = new ChromeDriver();
+                        break;
+                    }
+            }
+
+            driver.Manage().Window.Maximize();
+            return driver;
+        }
+
+    }
+}
---
+++ ate/ate/LoginTest.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
+
+namespace ate
+{
+    [TestFixture]
+    public class LoginTest
+    {
+        private const string _usermail = "david.smith.appulatetest@appulatemail.com";
+        private const string _userpass = "123321";
+
+        
+       
+        [TestCase (BrowsersEnum.chrome)]
+        [TestCase (BrowsersEnum.ie)]
+        public void Test1(BrowsersEnum browser)
+        {
+            using (IWebDriver driver = Driver.GetDriver(browser))
+            {
+
+                driver.Navigate().GoToUrl("https://appulatebeta.com/signin");
+                IWebElement useremail = driver.FindElement(By.Id("email"));
+                IWebElement userpassword = driver.FindElement(By.Id("password"));
+                IWebElement submitBttn = driver.FindElement(By.ClassName("signin-button"));
+
+                var halt = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+                useremail.Clear();
+                useremail.SendKeys(_usermail);
+
+                halt.Until(ExpectedConditions.TextToBePresentInElementValue(useremail, _usermail));
+
+                userpassword.Clear();
+                userpassword.SendKeys(_userpass);
+                submitBttn.Click();
+
+                string expectedUrl = "https://appulatebeta.com/icc/insuredclientpolicies.aspx";
+                Assert.IsTrue(driver.Url == expectedUrl, driver.Url);
+                driver.Dispose();
+            }
+        }
+    }
+}
